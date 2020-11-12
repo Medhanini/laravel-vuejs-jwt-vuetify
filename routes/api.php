cfb6e0fc91f1455df46c337b82c1eac65c944be1
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-11-12 19:28:54 +0100

    update logout jwt

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::group([
     Route::post('login', 'AuthController@login');
     Route::post('register', 'AuthController@register');
     Route::patch('update', 'AuthController@update');
-    Route::post('logout', 'AuthController@logout');
+    Route::post('logout', 'AuthController@logout')->name('api.jwt.logout');;
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 
