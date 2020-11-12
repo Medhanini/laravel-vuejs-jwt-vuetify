be9d944aa76e83d07b134258ed6b3eac50c06afa
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-11-12 19:11:23 +0100

    update logout jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,6 +65,7 @@ class AuthController extends Controller
                 'user' => auth()->user()
             ]
         ,200);
+        
    }
 
    /**
@@ -74,9 +75,16 @@ class AuthController extends Controller
     */
    public function logout()
    {
-       auth()->logout();
+    //    auth()->logout();
 
-       return response()->json(['message' => 'Successfully logged out']);
+    //    return response()->json(['message' => 'Successfully logged out']);
+
+       Auth::guard('api')->logout();
+
+       return response()->json([
+           'status' => 'success',
+           'message' => 'logout'
+       ], 200);
    }
 
    /**
